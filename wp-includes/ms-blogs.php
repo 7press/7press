8a6874bd9637b7b8cc24cda9f5bcd2c8bdecff0a
Pieter Hordijk <info@pieterhordijk.com>
2015-12-31 19:37:11 +0100

    Removed `ms_is_switched()`

--- wp-includes/ms-blogs.php
+++ wp-includes/ms-blogs.php
@@ -747,19 +747,6 @@ function restore_current_blog() {
 }
 
 /**
- * Determines if switch_to_blog() is in effect
- *
- * @since 3.5.0
- *
- * @global array $_wp_switched_stack
- *
- * @return bool True if switched, false otherwise.
- */
-function ms_is_switched() {
-	return ! empty( $GLOBALS['_wp_switched_stack'] );
-}
-
-/**
  * Check if a particular blog is archived.
  *
  * @since MU
