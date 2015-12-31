670457d397d66c208240393167bed6862552ff47
Pieter Hordijk <info@pieterhordijk.com>
2015-12-31 16:47:32 +0100

    Removed WP_USE_THEMES constant

--- wp-includes/template-loader.php
+++ wp-includes/template-loader.php
@@ -3,13 +3,13 @@
  * Loads the correct template based on the visitor's url
  * @package WordPress
  */
-if ( defined('WP_USE_THEMES') && WP_USE_THEMES )
-	/**
-	 * Fires before determining which template to load.
-	 *
-	 * @since 1.5.0
-	 */
-	do_action( 'template_redirect' );
+
+/**
+ * Fires before determining which template to load.
+ *
+ * @since 1.5.0
+ */
+do_action( 'template_redirect' );
 
 /**
  * Filter whether to allow 'HEAD' requests to generate content.
@@ -55,36 +55,35 @@ elseif ( is_embed() ) :
 	return;
 endif;
 
-if ( defined('WP_USE_THEMES') && WP_USE_THEMES ) :
-	$template = false;
-	if     ( is_404()            && $template = get_404_template()            ) :
-	elseif ( is_search()         && $template = get_search_template()         ) :
-	elseif ( is_front_page()     && $template = get_front_page_template()     ) :
-	elseif ( is_home()           && $template = get_home_template()           ) :
-	elseif ( is_post_type_archive() && $template = get_post_type_archive_template() ) :
-	elseif ( is_tax()            && $template = get_taxonomy_template()       ) :
-	elseif ( is_attachment()     && $template = get_attachment_template()     ) :
-		remove_filter('the_content', 'prepend_attachment');
-	elseif ( is_single()         && $template = get_single_template()         ) :
-	elseif ( is_page()           && $template = get_page_template()           ) :
-	elseif ( is_singular()       && $template = get_singular_template()       ) :
-	elseif ( is_category()       && $template = get_category_template()       ) :
-	elseif ( is_tag()            && $template = get_tag_template()            ) :
-	elseif ( is_author()         && $template = get_author_template()         ) :
-	elseif ( is_date()           && $template = get_date_template()           ) :
-	elseif ( is_archive()        && $template = get_archive_template()        ) :
-	elseif ( is_paged()          && $template = get_paged_template()          ) :
-	else :
-		$template = get_index_template();
-	endif;
-	/**
-	 * Filter the path of the current template before including it.
-	 *
-	 * @since 3.0.0
-	 *
-	 * @param string $template The path of the template to include.
-	 */
-	if ( $template = apply_filters( 'template_include', $template ) )
-		include( $template );
-	return;
+$template = false;
+if     ( is_404()            && $template = get_404_template()            ) :
+elseif ( is_search()         && $template = get_search_template()         ) :
+elseif ( is_front_page()     && $template = get_front_page_template()     ) :
+elseif ( is_home()           && $template = get_home_template()           ) :
+elseif ( is_post_type_archive() && $template = get_post_type_archive_template() ) :
+elseif ( is_tax()            && $template = get_taxonomy_template()       ) :
+elseif ( is_attachment()     && $template = get_attachment_template()     ) :
+	remove_filter('the_content', 'prepend_attachment');
+elseif ( is_single()         && $template = get_single_template()         ) :
+elseif ( is_page()           && $template = get_page_template()           ) :
+elseif ( is_singular()       && $template = get_singular_template()       ) :
+elseif ( is_category()       && $template = get_category_template()       ) :
+elseif ( is_tag()            && $template = get_tag_template()            ) :
+elseif ( is_author()         && $template = get_author_template()         ) :
+elseif ( is_date()           && $template = get_date_template()           ) :
+elseif ( is_archive()        && $template = get_archive_template()        ) :
+elseif ( is_paged()          && $template = get_paged_template()          ) :
+else :
+	$template = get_index_template();
 endif;
+
+/**
+ * Filter the path of the current template before including it.
+ *
+ * @since 3.0.0
+ *
+ * @param string $template The path of the template to include.
+ */
+if ( $template = apply_filters( 'template_include', $template ) )
+	include( $template );
+return;
